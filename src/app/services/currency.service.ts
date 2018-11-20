2a9bc8c7ebaad6283778c05fd50966d3606e5543
Felipe De la Fuente V <fadelafuente1@gmail.com>
2018-11-20 20:22:10 -0300

    add numbers to table and subnumbersToTable and show it on html

--- src/app/services/currency.service.ts
+++ src/app/services/currency.service.ts
@@ -3,6 +3,7 @@ import { HttpClient, HttpParams, HttpErrorResponse, HttpHeaders } from '@angular
 import { catchError } from 'rxjs/operators';
 import { throwError, Observable, BehaviorSubject} from 'rxjs';
 import { Currency } from '../models/currency';
+import _ from 'lodash';
 
 @Injectable({
   providedIn: 'root'
@@ -18,6 +19,8 @@ export class CurrencyService {
   castShiftCurrency = this.shiftCurrency.asObservable();
   currentShiftCurrencyAmount;
   calculatedCurrencies = {};
+  firstTableNumber = 1;
+  powerOf10Number = 1;
   apiUrl;
   constructor(private http: HttpClient) {
     this.apiUrl = 'https://api.exchangeratesapi.io';
@@ -30,7 +33,8 @@ export class CurrencyService {
         this.addCurrencyToCurrencyList(baseCurrency);
         this.addCalculatedCurrency(response.rates);
         this.loadCurrentShiftCurrencyAmount();
-        console.log(this.currentShiftCurrencyAmount);
+        this.loadNumbersToTable();
+        this.addSubrowToNumbersToTable();
       }, error => console.log(error)
     );
   }
@@ -83,6 +87,41 @@ export class CurrencyService {
   }
   private loadCurrentShiftCurrencyAmount() {
     this.currentShiftCurrencyAmount = this.calculatedCurrencies[this.baseCurrency.value][this.shiftCurrency.value];
+  }
+  private loadNumbersToTable () {
+    const numbersToTable = [];
+    for (const value of _.range(10)) {
+      const numbers = {};
+      const unit = value + 1;
+      const baseNumber = unit * this.firstTableNumber * Math.pow(10, this.powerOf10Number);
+      const shiftNumber = baseNumber * this.currentShiftCurrencyAmount;
+      numbers['baseNumber'] = baseNumber;
+      numbers['shiftNumber'] = shiftNumber.toFixed(2);
+      numbersToTable.push(numbers);
+    }
+    this.setNumbersToTable(numbersToTable);
+  }
+  private addSubrowToNumbersToTable () {
+    const numbersToTable = this.numbersToTable.value;
+    numbersToTable.forEach(
+      (row, index) => {
+        const subrowNumbersToTable = [];
+        subrowNumbersToTable.push({ 'baseNumber': row['baseNumber'], 'shiftNumber': row['shiftNumber'] });
+        for (const subrowValue of _.range(10)) {
+          const subrowNumbers = {};
+          const subrowUnit = subrowValue + 1;
+          const subrowPowerOf10Number = this.powerOf10Number - 1;
+          const subrowBaseNumber = row['baseNumber'] + subrowUnit * Math.pow(10, subrowPowerOf10Number);
+          const subrowShiftNumber = subrowBaseNumber * this.currentShiftCurrencyAmount;
+          subrowNumbers['baseNumber'] = subrowBaseNumber;
+          subrowNumbers['shiftNumber'] = subrowShiftNumber.toFixed(2);
+          subrowNumbersToTable.push(subrowNumbers);
+        }
+        numbersToTable[index]['clickead'] = false;
+        numbersToTable[index]['subrowNumbers'] = subrowNumbersToTable;
+      }
+    );
+    this.setNumbersToTable(numbersToTable);
   }
 
   errorHandler(error: HttpErrorResponse) {
