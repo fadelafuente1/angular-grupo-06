08eacb3e5ff7bdf59f1ac3f223efdfe9172df7d0
Felipe De la Fuente V <fadelafuente1@gmail.com>
2018-11-20 16:38:34 -0300

    createe models folder

--- src/app/services/currency.service.ts
+++ src/app/services/currency.service.ts
@@ -2,23 +2,23 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { catchError } from 'rxjs/operators';
 import { throwError, Observable } from 'rxjs';
+import { Currency } from '../models/currency';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ApiService {
+export class CurrencyService {
+
   apiUrl;
-  httpOptions = {
-    params: null
-  };
   constructor(private http: HttpClient) {
     this.apiUrl = 'https://api.exchangeratesapi.io';
   }
   getCurrency(baseCurrency: string): Observable<any> {
-    const url =  `${this.apiUrl}/latest`;
-    this.httpOptions.params = new HttpParams().set('base', baseCurrency);
-    return this.http.get(url, this.httpOptions).pipe(catchError(this.errorHandler));
+    const url =  `${this.apiUrl}/latest?base=${baseCurrency}`;
+    return this.http.get(url).pipe(catchError(this.errorHandler));
   }
+
+
   errorHandler(error: HttpErrorResponse) {
     return throwError(error.status  || 'Server Error');
   }
